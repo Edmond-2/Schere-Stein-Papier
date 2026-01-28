eaf0e79358cce778683657e60900ffec17d1947d
Edmond-2 <Edmondsh0511@outlook.de>
2026-01-28 16:44:34 +0100

    Add files via upload

---
+++ taschenrechner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welches kleine Einmaleins möchtest du sehen?");
+            int zahl = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                int ergebnis = zahl * i;
+                Console.WriteLine($"{zahl} x {i} = {ergebnis}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
